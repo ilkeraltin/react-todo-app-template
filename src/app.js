d9f8bf6e6f8dbedf5f42360670f3a69ac613758e
ilker ALTIN <ilkerobi@gmail.com>
2017-10-18 23:33:44 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+//import square, { add } from './utils';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import validator from 'validator';
+
+//const template = React.createElement('p',{}, 'testing!');
+
+const template = <p>this is reaect!</p>;
+
+ReactDOM.render(template, document.getElementById('app'));
---
+++ src/utils.js
@@ -0,0 +1,6 @@
+console.log('utils.js running!');
+
+const square = (x) => x * x;
+export const add = (x) => x + x;
+
+export default square;
