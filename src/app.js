a7196acc8cdd6dc392894342aa9069e9f8bf1125
ilker ALTIN <ilkerobi@gmail.com>
2017-10-20 09:28:33 +0300

    add style preprocessor, normalize.css, create global and compontent style structure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
 
 import IndecisionApp from './components/IndecisionApp';
 
